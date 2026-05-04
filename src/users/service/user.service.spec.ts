757ba14a40784971464d0e39ff98cb8b42dcd117
Fernando Benavides <fbenavides1991@gmail.com>
2026-05-04 16:56:05 -0500

    change

--- src/users/service/user.service.spec.ts
+++ src/users/service/user.service.spec.ts
@@ -33,7 +33,7 @@ describe('UserService', () => {
 
     expect(result).toEqual({
       id: 1,
-      name: 'Fernando',
+      name: 'Fernandos',
       email: 'fernando@test.com',
       password: '123456',
     });
